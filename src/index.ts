12b78562a1144c43de9a4bf3e8321311b388bc5d
mpaulson <the.primeagen@gmail.com>
2022-09-22 11:19:24 -0600

    to the startup

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { ISocket, Socket } from "./socket";
 import { ObjectPool } from "./pool";
 
 const server = new WebSocket.Server({
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     port: 42010,
 });
 
